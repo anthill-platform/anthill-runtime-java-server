eb0be7885afb0ec0e474052a0f7fbce6faa1a654
Alex Siryi <desertkun@gmail.com>
2017-05-20 23:30:34 +0300

    Version 0.1

---
+++ Readme.md
@@ -0,0 +1,97 @@
+# Server extention for Java Runtime for Anthill Platform
+
+This Runtime extends <a href="https://github.com/anthill-services/anthill-runtime-java">Java Runtime</a>,
+adding functionality for Game Server to communicate with
+<a href="https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#6-communication-between-game-server-and-controller-service">Controller Service</a>.
+
+## How To Use
+
+1. Override GameServerController class
+
+```java
+public class YourServerController extends GameServerController
+{
+    public YourServerController(String socket)
+    {
+        super(socket);
+    }
+
+    @Override
+    protected String getStatus()
+    {
+        return "ok";
+    }
+}
+```
+
+See <a href="https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#9-the-game-server-instance-status">this</a> for `getStatus()` method reference.
+
+2. Instantiate it in your Game Server instance
+
+```java
+ServerController = new YourServerController(sockets);
+```
+
+3. Once you Game Server instance initialized, call <a href="https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#initialized-request">Initialized Request</a>:
+
+```java
+ServerController.inited(settings, new GameServerController.InitedHandler()
+{
+    @Override
+    public void result(boolean success)
+    {
+        if (!success)
+        {
+            System.exit(-1);
+        }
+    }
+});
+```
+
+4. Once a Player connected, call `ServerController.joined` and one a Player left, call `ServerController.left`.
+
+## Installation
+
+#### Gradle
+
+1. Add the JitPack repository to your `build.gradle` file
+
+```
+allprojects {
+    repositories {
+        ...
+        maven { url "https://jitpack.io" }
+    }
+}
+```
+
+2. Add the dependency:
+
+```
+dependencies {
+    compile 'com.github.anthill-services:anthill-runtime-java-server:0.1'
+}
+```
+
+#### Maven
+
+1. Add the JitPack repository to your `pom.xml` file
+
+```xml
+<repositories>
+    <repository>
+        <id>jitpack.io</id>
+        <url>https://jitpack.io</url>
+    </repository>
+</repositories>
+```
+
+2. Add the dependency:
+
+```xml
+<dependency>
+    <groupId>com.github.anthill-services</groupId>
+    <artifactId>anthill-runtime-java-server</artifactId>
+    <version>0.1</version>
+</dependency>
+```
---
+++ src/org/anthillplatform/onlinelib/server/GameServerController.java
@@ -0,0 +1,313 @@
+package org.anthillplatform.onlinelib.server;
+
+import org.anthillplatform.onlinelib.entity.AccessToken;
+import org.anthillplatform.onlinelib.services.GameService;
+import org.anthillplatform.onlinelib.services.LoginService;
+import org.anthillplatform.onlinelib.util.JsonRPC;
+import org.json.JSONObject;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+import java.nio.charset.Charset;
+
+/**
+ * Game Server Controller
+ *
+ * This class manages communication with the Controller Service using ZeroMQ library for transport-level operations,
+ * and JSON-RPC protocol for actual requests made to the Controller Service.
+ *
+ * @author desertkun
+ */
+
+public abstract class GameServerController
+{
+    private final ZMQ.Socket socket;
+    private final ZContext context;
+    private JsonRPC rpc;
+
+    public interface InitedHandler
+    {
+        void result(boolean success);
+    }
+
+    public interface PlayerJoinedHandler
+    {
+        void result(boolean success, AccessToken token);
+    }
+
+    public interface PlayerLeftHandler
+    {
+        void result(boolean success);
+    }
+
+    public interface UpdateSettingsHandler
+    {
+        void result(boolean success);
+    }
+
+    public interface CheckDeploymentHandler
+    {
+        void result(boolean success);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    protected abstract String getStatus();
+
+    @SuppressWarnings("WeakerAccess")
+    protected void logInfo(String log)
+    {
+        System.out.println(log);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    protected void logError(String log)
+    {
+        System.err.println(log);
+    }
+
+    public GameServerController(String socket)
+    {
+        logInfo("Listening from Controller Service on " + socket);
+
+        this.context = new ZContext();
+        this.socket = context.createSocket(ZMQ.PAIR);
+        this.socket.connect("ipc://" + socket);
+
+        this.rpc = new JsonRPC()
+        {
+            @Override
+            protected void send(String data)
+            {
+                GameServerController.this.socket.send(data, ZMQ.NOBLOCK);
+            }
+
+            @Override
+            public void onError(int code, String message, String data)
+            {
+                logError("Error: " + code + " " + message + " " + data);
+            }
+        };
+    }
+
+    /**
+     * The main update cycle method. Need to be called in the main process loop
+     */
+    public void update()
+    {
+        String data;
+
+        while ((data = socket.recvStr(ZMQ.NOBLOCK, Charset.defaultCharset())) != null)
+        {
+            received(data);
+        }
+    }
+
+    public void stop()
+    {
+        context.destroy();
+    }
+
+    public JsonRPC getRpc()
+    {
+        return rpc;
+    }
+
+    private void received(String data)
+    {
+        getRpc().received(data);
+    }
+
+    public void joined(String key, PlayerJoinedHandler handler)
+    {
+        joined(key, null, null, handler);
+    }
+
+    /**
+     * Performs 'Player Joined Request'
+     *
+     * See https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#player-joined-request
+     *
+     * @param key The registration Key
+     * @param extendToken If both extend_token and extend_scopes are passed diring the joined request, the Access Token
+     *                    of the player will be extended using extend_token as master token and extend_scopes as a
+     *                    list of scopes the Player's Access Token should be extended with.
+     * @param extendScopes see extendToken
+     * @param handler Callback with call response
+     */
+    public void joined(String key, AccessToken extendToken, String extendScopes, final PlayerJoinedHandler handler)
+    {
+        LoginService loginService = LoginService.get();
+
+        if (loginService == null)
+            throw new RuntimeException("No login service!");
+
+        JSONObject params = new JSONObject();
+
+        params.put("key", key);
+
+        if (extendToken != null && extendScopes != null)
+        {
+            params.put("extend_token", extendToken.getToken());
+            params.put("extend_scopes", extendScopes);
+        }
+
+        getRpc().request("joined", new JsonRPC.ResponseHandler()
+        {
+            @Override
+            public void success(Object response)
+            {
+                String token = ((JSONObject) response).getString("access_token");
+                handler.result(true, new AccessToken(token));
+            }
+
+            @Override
+            public void error(int code, String message, String data)
+            {
+                logError("Error while joining: " + code + " " + message + " " + data);
+                handler.result(false, null);
+            }
+        }, params);
+    }
+
+    /**
+     * Performs 'Player Left Request'
+     *
+     * See https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#player-left-request
+     *
+     * @param key The registration Key
+     * @param handler Callback with call response
+     */
+    public void left(String key, final PlayerLeftHandler handler)
+    {
+        JSONObject params = new JSONObject();
+
+        params.put("key", key);
+
+        getRpc().request("left", new JsonRPC.ResponseHandler()
+        {
+            @Override
+            public void success(Object response)
+            {
+                handler.result(true);
+            }
+
+            @Override
+            public void error(int code, String message, String data)
+            {
+                logError("Error to leave the player: " + code + " " + message + " " + data);
+                handler.result(false);
+            }
+        }, params);
+    }
+
+    /**
+     * Performs 'Update Room Settings Request'
+     *
+     * See https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#update-room-settings-request
+     *
+     * @param settings New settings for the Room
+     * @param handler Callback with call response
+     */
+    public void updateSettings(GameService.RoomSettings settings, final UpdateSettingsHandler handler)
+    {
+        JSONObject params = new JSONObject();
+
+        params.put("settings", settings.getSettings());
+
+        getRpc().request("update_settings", new JsonRPC.ResponseHandler()
+        {
+            @Override
+            public void success(Object response)
+            {
+                handler.result(true);
+            }
+
+            @Override
+            public void error(int code, String message, String data)
+            {
+                logError("Error to update settings: " + code + " " + message + " " + data);
+                handler.result(false);
+            }
+        }, params);
+    }
+
+    /**
+     * Performs 'Check Game Server Deployment Request'
+     *
+     * See https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#check-game-server-deployment-request
+     *
+     * @param handler Callback with call response
+     */
+    public void checkDeployment(final CheckDeploymentHandler handler)
+    {
+        JSONObject params = new JSONObject();
+
+        getRpc().request("check_deployment", new JsonRPC.ResponseHandler()
+        {
+            @Override
+            public void success(Object response)
+            {
+                logError("Deployment is up to date.");
+                handler.result(true);
+            }
+
+            @Override
+            public void error(int code, String message, String data)
+            {
+                logError("Deployment check failed: " + code + " " + message + " " + data);
+                handler.result(false);
+            }
+        }, params);
+    }
+
+    public void inited(final InitedHandler handler)
+    {
+        inited(null, handler);
+    }
+
+    /**
+     * Performs 'Initialized Request'
+     *
+     * See https://github.com/anthill-services/anthill-game/blob/master/doc/API.md#initialized-request
+     *
+     * @param settings (Optional) Update room settings along with initialization
+     * @param handler Callback with call response
+     */
+    public void inited(GameService.RoomSettings settings, final InitedHandler handler)
+    {
+        getRpc().addHandler("status", new JsonRPC.MethodHandler()
+        {
+            @Override
+            public Object called(Object params) throws JsonRPC.JsonRPCException
+            {
+                JSONObject status = new JSONObject();
+                status.put("status", getStatus());
+                return status;
+            }
+        });
+
+        JSONObject params = new JSONObject();
+
+        if (settings != null)
+        {
+            params.put("settings", settings.getSettings());
+        }
+
+        getRpc().request("inited", new JsonRPC.ResponseHandler()
+        {
+            @Override
+            public void success(Object response)
+            {
+                logError("Inited: " + response.toString());
+                handler.result(true);
+            }
+
+            @Override
+            public void error(int code, String message, String data)
+            {
+                logError("Error while inited: " + code + " " + message + " " + data);
+                handler.result(false);
+            }
+        }, params);
+    }
+}
